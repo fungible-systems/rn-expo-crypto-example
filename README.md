82a61c8884bef8fc00da361dcc9dc5f70ca3ef20
aulneau <aulneau@users.noreply.github.com>
2022-11-14 09:46:07 -0600

    show slowness

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 - Follow expo instructions to get your system set up: https://docs.expo.dev/get-started/installation/
 - `yarn install`
-- `yarn expo:prebuild`
-- `yarn ios`
+- `yarn start`
+- hit `i` to run the ios simulator
 
+
+To see speed difference between `hermes` and `jsc` remove the `jsEngine` field in `app.json`
--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,24 @@
-import {Text} from "react-native";
+import {Button, Text} from "react-native";
 import * as React from 'react'
-import * as secp from '@noble/secp256k1';
+import {HDKey} from "@scure/bip32";
+import {generateMnemonic, mnemonicToSeedSync} from "@scure/bip39";
+import {wordlist} from '@scure/bip39/wordlists/english'
 
-
-// from readme
-async function testFn () {
-    // keys, messages & other inputs can be Uint8Arrays or hex strings
-    // Uint8Array.from([0xde, 0xad, 0xbe, 0xef]) === 'deadbeef'
-    const privKey = secp.utils.randomPrivateKey();
-    const pubKey = secp.getPublicKey(privKey);
-    // @ts-ignore, wrong input, should be byte array
-    const msgHash = await secp.utils.sha256('hello world');
-    const signature = await secp.sign(msgHash, privKey);
-    const isValid = secp.verify(signature, msgHash, pubKey);
-
-    // Schnorr signatures
-    const rpub = secp.schnorr.getPublicKey(privKey);
-    const rsignature = await secp.schnorr.sign(msgHash, privKey);
-    const risValid = await secp.schnorr.verify(rsignature, msgHash, rpub);
-    // log the results
-    console.log(privKey, pubKey, signature, isValid, risValid)
+const makeKey = () => {
+    console.log('starting key')
+    const start = performance.now()
+    HDKey.fromMasterSeed(mnemonicToSeedSync(generateMnemonic(wordlist)))
+    const end = performance.now()
+    console.log(`Creating a key took ${end - start} ms.`)
 }
+
 export default function Page() {
-    // run the test fn
-    void testFn()
-    return <React.Fragment><Text>Demo page</Text></React.Fragment>;
+    return <React.Fragment>
+        <Text>Demo page</Text>
+        <Button title={"empty"}
+        />
+        <Button title={"Make key"}
+                onPress={() => {makeKey()}}
+        />
+    </React.Fragment>;
 }
